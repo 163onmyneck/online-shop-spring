1951fffa88b05908577ad361509568248747f40d
Oleh <mihailyakyb45@gmail.com>
2024-03-27 22:27:38 +0200

    Replaced the existing BookRepository with the one that will use JpaRepository interface, Added Liquibase support, Changed spring.jpa.hibernate.ddl-auto property value to validate, Implemented soft delete concept for the Book entity, Added missing endpoints and methods for BookService

--- src/main/java/com/example/demo/service/BookService.java
+++ src/main/java/com/example/demo/service/BookService.java
@@ -10,4 +10,8 @@ public interface BookService {
     List<BookDto> findAll();
 
     BookDto getById(Long id);
+
+    void update(CreateBookRequestDto createBookRequestDto, Long id);
+
+    void deleteById(Long id);
 }
