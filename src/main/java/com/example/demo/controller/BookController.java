22ff413ff548ad8523aa8367894f9faf92871070
Oleh <mihailyakyb45@gmail.com>
2024-03-27 22:27:38 +0200

    Replaced the existing BookRepository with the one that will use JpaRepository interface, Added Liquibase support, Changed spring.jpa.hibernate.ddl-auto property value to validate, Implemented soft delete concept for the Book entity, Added missing endpoints and methods for BookService

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -5,9 +5,11 @@ import com.example.demo.dto.CreateBookRequestDto;
 import com.example.demo.service.BookService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,4 +35,16 @@ public class BookController {
     public BookDto createBook(@RequestBody CreateBookRequestDto bookDto) {
         return bookService.save(bookDto);
     }
+
+    @PutMapping("/{id}")
+    public void updateBook(@RequestBody CreateBookRequestDto bookRequestDto,
+                           @PathVariable Long id) {
+        bookService.update(bookRequestDto, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Long id) {
+        bookService.deleteById(id);
+    }
+
 }
--- src/main/java/com/example/demo/service/impl/BookServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/BookServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.demo.dto.BookDto;
 import com.example.demo.dto.CreateBookRequestDto;
 import com.example.demo.exception.EntityNotFoundException;
 import com.example.demo.mapper.BookMapper;
+import com.example.demo.model.Book;
 import com.example.demo.repository.BookRepository;
 import com.example.demo.service.BookService;
 import java.util.List;
@@ -32,5 +33,39 @@ public class BookServiceImpl implements BookService {
     public BookDto getById(Long id) {
         return bookMapper.toDto(bookRepository.findById(id).orElseThrow(
                 () -> new EntityNotFoundException("Can not get book by id:" + id)));
+    }
+
+    @Override
+    public void update(CreateBookRequestDto createBookRequestDto, Long id) {
+        Book book = bookRepository.findById(id).orElseThrow(() ->
+            new EntityNotFoundException("Can not get book by id:" + id));
+        setFields(createBookRequestDto, book);
+        bookRepository.save(book);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        bookRepository.deleteById(id);
+    }
+
+    private void setFields(CreateBookRequestDto createBookRequestDto, Book book) {
+        if (createBookRequestDto.getTitle() != null) {
+            book.setTitle(createBookRequestDto.getTitle());
+        }
+        if (createBookRequestDto.getAuthor() != null) {
+            book.setAuthor(createBookRequestDto.getAuthor());
+        }
+        if (createBookRequestDto.getIsbn() != null) {
+            book.setIsbn(createBookRequestDto.getIsbn());
+        }
+        if (createBookRequestDto.getPrice() != null) {
+            book.setPrice(createBookRequestDto.getPrice());
+        }
+        if (createBookRequestDto.getDescription() != null) {
+            book.setDescription(createBookRequestDto.getDescription());
+        }
+        if (createBookRequestDto.getCoverImage() != null) {
+            book.setCoverImage(createBookRequestDto.getCoverImage());
+        }
     }
 }
