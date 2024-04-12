d7cf9543b4ec3275beffce0ebe16859dbb5ca493
Oleh <mihailyakyb45@gmail.com>
2024-04-12 17:00:42 +0300

    added Roles

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -5,15 +5,24 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name = "users")
 @Getter
 @Setter
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -26,4 +35,41 @@ public class User {
     @Column(nullable = false)
     private String lastName;
     private String shippingAddress;
+    @ManyToMany
+    @JoinTable(name = "user_roles",
+                joinColumns = @JoinColumn(name = "user_id"),
+                inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+            .map(role -> new SimpleGrantedAuthority(role.getRoleName().name()))
+            .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/com/example/demo/service/user/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/user/impl/UserServiceImpl.java
@@ -4,16 +4,23 @@ import com.example.demo.dto.user.UserRegistrationRequestDto;
 import com.example.demo.dto.user.UserResponseDto;
 import com.example.demo.exception.RegistrationException;
 import com.example.demo.mapper.UserMapper;
+import com.example.demo.model.User;
+import com.example.demo.repository.role.RoleRepository;
 import com.example.demo.repository.user.UserRepository;
 import com.example.demo.service.user.UserService;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+    private static final long USER_ID = 1;
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
     @Override
     public UserResponseDto register(UserRegistrationRequestDto requestDto)
@@ -23,7 +30,10 @@ public class UserServiceImpl implements UserService {
         }
 
         if (userRepository.findByEmail(requestDto.getEmail()).isEmpty()) {
-            return userMapper.toDto(userRepository.save(userMapper.toModel(requestDto)));
+            requestDto.setPassword(passwordEncoder.encode(requestDto.getPassword()));
+            User user = userMapper.toModel(requestDto);
+            user.setRoles(Set.of(roleRepository.findById(USER_ID).get()));
+            return userMapper.toDto(userRepository.save(user));
         }
         throw new RegistrationException("Can not register user with email: "
             + requestDto.getEmail() + " User with this email already exists");
