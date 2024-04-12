ed7d74333aff381a916288bd4610cc347476e001
Oleh <mihailyakyb45@gmail.com>
2024-04-12 20:09:36 +0300

    fixed

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(
                 auth -> auth
-                    .requestMatchers("/auth/**","/swagger-ui/**")
+                    .requestMatchers("/auth/**", "/swagger-ui/**")
                     .permitAll()
                     .anyRequest()
                     .authenticated()
