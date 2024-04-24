47145df4c18bbb6301199923a4653201af7d7732
Oleh <mihailyakyb45@gmail.com>
2024-04-25 00:19:41 +0300

    added fixes

--- src/main/java/com/example/demo/repository/shopping/cart/ShoppingCartRepository.java
+++ src/main/java/com/example/demo/repository/shopping/cart/ShoppingCartRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
-    @Query("FROM ShoppingCart sc WHERE sc.user.email = :email")
+    @Query("SELECT sc FROM ShoppingCart sc JOIN sc.user u WHERE u.email = :email")
     Optional<ShoppingCart> findByUserEmail(@Param("email") String email);
 }
